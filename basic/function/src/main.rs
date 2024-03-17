12c95f0156331f50d41c4822cdce5ea4c0a6b592
ag502 <ag502@naver.com>
2024-03-18 06:42:48 +0900

    add: function basic

---
+++ basic/function/src/main.rs
@@ -0,0 +1,22 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+fn main() {
+    show_first_name();
+    show_last_name();
+}
+
+fn show_first_name() {
+    println!("Jaeyun");
+}
+
+fn show_last_name() {
+    println!("Lee");
+}
